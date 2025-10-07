a04006444db1d02d2ebc644a4f87c8ef74d4617e
gadaparathanbabu-oresv <gadaparathanbabu@gmail.com>
2025-10-07 14:58:28 +0530

    Add files via upload

--- api/send-order.js
+++ api/send-order.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
         const transporter = nodemailer.createTransport({
             host: "smtp-relay.brevo.com",
             port: 587,
+            secure: false, // use TLS
             auth: {
                 user: "98b923001@smtp-brevo.com", // your Mailtrap user
                 pass: "zdTgcAwrFRYsQPXm", // your Mailtrap password
@@ -31,10 +32,7 @@ export default async function handler(req, res) {
         });
 
         await transporter.sendMail({
-            from: {
-                name: "Neuskale Title",
-                address: "laxmir22@10008867.brevosend.com", // verified
-            },
+            from: `"Neuskale Title Orders" <YOUR_VERIFIED_SENDER_EMAIL>`,
             to: "rathan@ventois.com",
             subject: `New Title Order - ${state}, ${county}`,
             text: `
